af382f549159a8943aca0c351f26af64fd78cb4b
Divyansh Singh <divyansh1029384756@gmail.com>
2025-09-08 00:05:50 +0530

    set env

--- backend_1/src/index.ts
+++ backend_1/src/index.ts
@@ -1,14 +1,27 @@
-// make webSocket
-import { WebSocketServer } from 'ws';
-import { GameManager } from './GameManager';
+import http from "http";
+import { WebSocketServer } from "ws";
+import { GameManager } from "./GameManager";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
-const wss = new WebSocketServer({ port: PORT });
+const server = http.createServer((req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200);
+    res.end("OK");
+  } else {
+    res.writeHead(200);
+    res.end("Chess backend is running");
+  }
+});
+
+const wss = new WebSocketServer({ server });
 
 const gameManager = new GameManager();
-
-wss.on('connection', function connection(ws) {
+wss.on("connection", (ws) => {
   gameManager.addUser(ws);
-  ws.on('disconnect', ()=> gameManager.removeUser(ws) )
+  ws.on("close", () => gameManager.removeUser(ws));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
