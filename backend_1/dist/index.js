6f39f82c3d0b60c7467980f60030f3e822985051
Divyansh Singh <divyansh1029384756@gmail.com>
2025-09-07 21:33:53 +0530

    built

--- backend_1/dist/index.js
+++ backend_1/dist/index.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // make webSocket
 const ws_1 = require("ws");
 const GameManager_1 = require("./GameManager");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const wss = new ws_1.WebSocketServer({ port: PORT });
 const gameManager = new GameManager_1.GameManager();
 wss.on('connection', function connection(ws) {
     gameManager.addUser(ws);
